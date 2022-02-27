2017500f75722dd16e4af16dc136789ec00e9672
thsanggiii <anggitaajeng307@gmail.com>
2022-02-27 19:28:04 +0700

    add statement program

---
+++ TaskSection4-IntroductiontoAlgorithmandDartPrograming/section4_praktikum/bin/section4_statement_program.dart
@@ -0,0 +1,9 @@
+main() {
+  print("PROGRAM MENAMPILKAN KALIMAT");
+  //deklarasi variabel
+  String namaDepan = 'Anggita';
+  String namaTengah = 'Ajeng' ;
+  String namaBelakang = 'Karina';
+  //perintah menampilkan output
+  print('Perkenalkan namaku $namaDepan $namaTengah $namaBelakang');
+}
