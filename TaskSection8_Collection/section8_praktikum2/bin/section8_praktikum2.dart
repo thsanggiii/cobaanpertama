01c6287f4eb7fc6dfc3519ea1484a44a702b6d88
thsanggiii <anggitaajeng307@gmail.com>
2022-03-07 13:32:01 +0700

    add project section 8

---
+++ TaskSection8_Collection/section8_praktikum2/bin/section8_praktikum2.dart
@@ -0,0 +1,9 @@
+void main() {
+  var jumlahpenduduk = {};
+  jumlahpenduduk['Kabupaten Kediri'] = [123,95,73,70];
+  jumlahpenduduk['Kabupaten Probolinggo'] = [150,95,43,45];
+  jumlahpenduduk['Kabupaten Malang'] = [56,54,60,34];
+  jumlahpenduduk['Madura'] = [89,78,78,79];
+  print('Jumlah data penerima bantuan sosial dalam 4 tahun terakhir: ');
+  print(jumlahpenduduk);
+}
