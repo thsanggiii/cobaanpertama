43fcd375fed7d793f5ee31e67fdf13bcdfac3957
thsanggiii <anggitaajeng307@gmail.com>
2022-03-25 21:58:05 +0700

    add section 13

---
+++ TaskSection13_CLIandFLutterPackageManagement/section13_praktikum/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Project Section 12'),
+      
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          
+          children: <Widget>[
+            Chip(
+              padding: EdgeInsets.all(0),
+              backgroundColor: Colors.deepPurple,
+              label: Text('BADGE', style: TextStyle(color: Colors.white)),
+              ),
+              Badge(
+              toAnimate: false,
+              shape: BadgeShape.square,
+              badgeColor: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(8),
+              badgeContent: Text('BADGE', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
